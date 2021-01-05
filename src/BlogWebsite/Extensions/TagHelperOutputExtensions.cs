b1602a3a30930f157f2256742a8f764b5f6db775
Sander van 't Einde <sandervanteinde@gmail.com>
2021-01-05 21:40:45 +0100

    Initial setup for the blogging website

---
+++ src/BlogWebsite/Extensions/TagHelperOutputExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.AspNetCore.Razor.TagHelpers
+{
+    public static class TagHelperOutputExtensions
+    {
+        public static void AddClass(this TagHelperOutput tagHelper, params string[] classes)
+        {
+            var newClasses = new HashSet<string>();
+            if (tagHelper.Attributes.TryGetAttribute("class", out var attribute))
+                foreach (var @class in attribute.Value.ToString().Split(" "))
+                    newClasses.Add(@class);
+
+            foreach (var @class in classes)
+                newClasses.Add(@class);
+
+            tagHelper.Attributes.SetAttribute("class", string.Join(' ', newClasses));
+        }
+    }
+}
